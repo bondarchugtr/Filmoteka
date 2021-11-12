94dfc856f27f5d9516090528828697fd4c428bae
bondarchugtr <81237327+bondarchugtr@users.noreply.github.com>
2021-11-12 17:01:47 +0200

    +

--- src/js/components/filter.js
+++ src/js/components/filter.js
@@ -7,9 +7,11 @@ const main = document.querySelector('.hero')
 
 let releaseDateDesc = 'primary_release_date.desc';
 let releaseDateAsk = 'primary_release_date.asc';
-let popularity = 'popularity.asc';
+let popularityDesc = 'popularity.desc';
+let popularityAsc = 'popularity.asc';
 
-
+let voteAverageAsc = 'vote_average.asc';
+let voteAverageDesc = 'vote_average.desc'
 //раскрытие списка сортировки
 
 const sort = document.querySelector('.filter-muvie__list');
@@ -17,12 +19,29 @@ const sort = document.querySelector('.filter-muvie__list');
 function onOpenSort(evt) {
     evt.preventDefault()
     console.log('object');
+}
 
+
+function filterPopularityDesc() {
+    filterGlobal(popularityDesc)
+        .then(data => {
+            renderMovies(data.results)
+        }).catch(() => {
+            alert("error");
+        });
+}
+function filterPopularityAsc() {
+    filterGlobal('', popularityAsc)
+        .then(data => {
+            renderMovies(data.results)
+        }).catch(() => {
+            alert("error");
+        });
 }
 
 
 function filterReleaseDesc() {
-    filterGlobal(releaseDateDesc, '')
+    filterGlobal('', '', releaseDateDesc)
         .then(data => {
             renderMovies(data.results)
         }).catch(() => {
@@ -31,7 +50,15 @@ function filterReleaseDesc() {
 }
 
 function filterReleaseAsk() {
-    filterGlobal('', releaseDateAsk)
+    filterGlobal('', '', '', releaseDateAsk)
+        .then(data => {
+            renderMovies(data.results)
+        }).catch(() => {
+            alert("error");
+        });
+}
+function filterReleaseAsk() {
+    filterGlobal('', '', '', '', releaseTitleDesc)
         .then(data => {
             renderMovies(data.results)
         }).catch(() => {
@@ -39,25 +66,32 @@ function filterReleaseAsk() {
         });
 }
 
+
+
 function filterMain(data) {
     const marcup = filter(data)
     main.insertAdjacentHTML("beforeend", marcup);
-    const filterList = document.querySelectorAll('.filter-list__item')
-    filterList.forEach(el => {
-        el.addEventListener('click', e => {
-            e.preventDefault()
-            const linck = e.target.dataset.atribute
-            if (linck === "release-date-desc") {
-                filterReleaseDesc()
-            } else if (linck === "release-date-ask") {
-                filterReleaseAsk()
-            }
-        })
+    const filterList = document.querySelector('.filter-list')
+    console.log(filterList);
+    // filterList.forEach(el => {
+    filterList.addEventListener('click', e => {
+        e.preventDefault()
+        console.log(e.target);
+        const linck = e.target.dataset.atribute
+        if (linck === "release-date-desc") {
+            filterReleaseDesc()
+        } else if (linck === "release-date-ask") {
+            filterReleaseAsk()
+        } else if (linck === "popularity-desc") {
+            filterPopularityDesc()
+        } else if (linck === "popularity-asc") {
+            filterPopularityAsc()
+        }
     })
-    // console.log(marcup);
+    // })
 }
 
-// filterMain()
+filterMain()
 
 
 
