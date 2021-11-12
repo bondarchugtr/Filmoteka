1896ab2621408469c2167d89f4ea89969699e403
bondarchugtr <81237327+bondarchugtr@users.noreply.github.com>
2021-11-12 22:54:35 +0200

    Update modal_one_movie.js

--- src/js/layout/modal_one_movie.js
+++ src/js/layout/modal_one_movie.js
@@ -1,6 +1,42 @@
 import modal_one_movie_markup from '../../views/partials/modal_one_movie.hbs';
-import testcard from '../testcard.json'; //test object with movie detail. delete after start function
 import { renderModal } from '../components/modal';
-const modalContent = modal_one_movie_markup(testcard); //need insert object with movie detail
+import { renderParamsCard } from '../components/fetch'
 
-//renderModal(modalContent); //function open modal with string html. test
+
+let id = "id";
+let linksID = [];
+
+const test = document.querySelector('.modal__content')
+
+function renderParamCard(data) {
+  const marcup = modal_one_movie_markup(data);
+  test.innerHTML = marcup;
+}
+
+function renderMovieSeorchParam() {
+  renderParamsCard(id)
+    .then((data) => {
+      renderModal()
+      renderParamCard(data)
+    })
+    .catch(() => {
+    });
+}
+const imagesRef = document.querySelector('.gallery-list li');
+console.log(imagesRef);
+
+function seorchId() {
+  const imagesRef = document.querySelector('.gallery-list');
+  imagesRef.addEventListener('click', e => {
+    e.preventDefault()
+    console.log(e.target.parentNode);
+    if (e.target.nodeName === 'UL') {
+      console.log('Выход');
+      return;
+    }
+    // id = e.target.closest('.gallery-list__item').dataset.id;
+    console.log(id)
+    renderMovieSeorchParam(id)
+  })
+}
+seorchId()
